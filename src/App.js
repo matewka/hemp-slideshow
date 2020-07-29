1bdd56ada73fe52ca1b85a7dd81ec20faee33a2d
Mateusz Gawlinski <mateusz.gawlinski@twill.net>
2020-07-29 20:02:37 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel className="carousel" autoPlay infiniteLoop showArrows={false} showStatus={false}
+                showIndicators={false} showThumbs={false} stopOnHover={false} interval={6000}>
+        <Slide src="https://picsum.photos/200/300"/>
+        <Slide src="https://picsum.photos/300/200"/>
+        <Slide src="https://picsum.photos/200/500"/>
+        <Slide src="https://picsum.photos/200/450"/>
+        <Slide src="https://picsum.photos/400/500"/>
+        <Slide src="https://picsum.photos/400/700"/>
+      </Carousel>
     </div>
   );
+}
+
+const Slide = (props) => {
+  return <div className="slide" style={{ backgroundImage: `url(${props.src})` }} />
 }
 
 export default App;
